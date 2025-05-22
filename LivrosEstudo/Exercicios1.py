a7218fcdae134f910166cbb6962a90c29870b7ee
LARISSA GOMES <25.00625-5@maua.br>
2025-05-22 12:51:37 -0300

    livro python estudo cap 1 e 2

---
+++ LivrosEstudo/Exercicios1.py
@@ -0,0 +1,15 @@
+###  1) CONVERTA AS SEGUINTES EXPRESSÕES MATEMATICAS PARA QUE POSSAM SER CALCULADAS NO PYTHON ###
+'''
+10+20X30
+4^2 /30
+(9^4+2)X6-1
+'''
+print(10+20*30)
+print(4**2/30)
+print((9**4+2)*6-1)
+
+'''CALCULAR O SALARIO COM O AUMENTO'''
+salario = 1500
+aumento = 5
+
+print(salario + (salario*aumento/100))
